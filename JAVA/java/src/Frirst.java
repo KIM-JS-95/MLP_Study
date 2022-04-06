2cfe272a3683cfa7b29817dabf3af709693f9c71
KIM-JS-95 <baugh248730@gmail.com>
2022-04-06 17:48:26 +0900

    123

---
+++ JAVA/java/src/Frirst.java
@@ -0,0 +1,8 @@
+public class Frirst {
+    public static void main(String[] args) {
+        Second second = new Second();
+        second.setName("hi");
+        System.out.println(second.getName());
+    }
+}
+
---
+++ JAVA/java/src/Second.java
@@ -0,0 +1,15 @@
+public class Second {
+
+    // 캡슐화되어잇는 요소에 접근하려면?
+    private String name;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
